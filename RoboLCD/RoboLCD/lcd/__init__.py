947cc153a8dc8a76acf6bba786eba5da1b61b583
mcecchi <mauro.cecchi@met-informatica.it>
2017-09-10 17:41:33 +0200

    Enabled bed leveling wizard

--- RoboLCD/RoboLCD/lcd/__init__.py
+++ RoboLCD/RoboLCD/lcd/__init__.py
@@ -504,7 +504,9 @@ def start():
         bed_calib = Robo_Icons('Icons/Bed_Calibration/Bed placement.png', roboprinter.lang.pack['RoboIcons']['Bed_Cal'], 'BED_CALIBRATION')
         buttons = [fc, fl, z, slicer, bed_calib, fine_tune]
       else:
-        buttons = [fc, fl, z, slicer, fine_tune]
+        bed_calib = Robo_Icons('Icons/Bed_Calibration/Bed placement.png', roboprinter.lang.pack['RoboIcons']['Bed_Cal'], 'BED_CALIBRATION')
+        #buttons = [fc, fl, z, slicer, fine_tune]
+        buttons = [fc, fl, z, slicer, bed_calib, fine_tune]
 
 
       c = Scroll_Box_Icons(buttons)
